a6d1c685b9cec95121398b4c876e880f44a65bd2
Oray KURT <oraykt@gmail.com>
2021-02-10 15:56:12 +0100

    global exception handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotDestroyed, with: :not_destroyed
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  def not_destroyed error
+    render json: error, status: :unprocessable_entity
+  end
+
+  def not_found error
+    render json: error, status: :not_found
+  end
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,4 @@
 class BooksController < ApplicationController
-  rescue_from ActiveRecord::RecordNotDestroyed, with: :not_destroyed
-  rescue_from ActiveRecord::RecordNotFound, with: :not_found
   def index
     render json: Book.all
   end
@@ -30,13 +28,5 @@ class BooksController < ApplicationController
 
   def find_book
     Book.find(params.require(:id))
-  end
-
-  def not_destroyed error
-    render json: error, status: :unprocessable_entity
-  end
-
-  def not_found error
-    render json: error, status: :not_found
   end
 end
