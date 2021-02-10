d942cb794db6dea64145b171f6aded504208074e
Oray KURT <oraykt@gmail.com>
2021-02-10 15:15:21 +0100

    books#create, Book.author_id : optional

---
+++ db/migrate/20210210141244_change_author_id_to_be_optional_for_book.rb
@@ -0,0 +1,5 @@
+class ChangeAuthorIdToBeOptionalForBook < ActiveRecord::Migration[6.1]
+  def change
+    change_column :books, :author_id, :integer, null: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_10_134820) do
+ActiveRecord::Schema.define(version: 2021_02_10_141244) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2021_02_10_134820) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.integer "author_id", null: false
+    t.integer "author_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["author_id"], name: "index_books_on_author_id"
