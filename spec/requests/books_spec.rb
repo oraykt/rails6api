d2c47d6bde08708a2ea2322181c61dd4bb9dffa3
Oray KURT <oraykt@gmail.com>
2021-02-11 00:50:18 +0100

    Pagination /api/v1/books with Unit Test

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -7,10 +7,11 @@ describe 'Books API', type: :request do
   let(:second_author) { FactoryBot.create(:author, name: 'Second Author') }
 
   describe 'GET /books' do
-    it 'returns all books' do
+    before do
       FactoryBot.create(:book, title: 'Test Title 1', description: 'Test Description 1', author: first_author)
       FactoryBot.create(:book, title: 'Test Title 2', description: 'Test Description 2', author: second_author)
-
+    end
+    it 'returns all books' do
       get '/api/v1/books'
       expect(response).to have_http_status(:ok)
       expect(response_body.size).to eq(2)
@@ -30,6 +31,44 @@ describe 'Books API', type: :request do
           }
         ]
       )
+    end
+
+    it 'return a subset of books based on limit' do
+      get '/api/v1/books', params: { limit: 1 }
+      expect(response).to have_http_status(:ok)
+      expect(response_body.size).to eq(1)
+      expect(response_body).to eql(
+        [
+          {
+            'id' => 1,
+            'title' => 'Test Title 1',
+            'description' => 'Test Description 1',
+            'author' => 'First Author'
+          }
+        ]
+      )
+    end
+
+    it 'return a subset of books based on limit and offset' do
+      get '/api/v1/books', params: { limit: 1, offset: 1 }
+      expect(response).to have_http_status(:ok)
+      expect(response_body.size).to eq(1)
+      expect(response_body).to eql(
+        [
+          {
+            'id' => 2,
+            'title' => 'Test Title 2',
+            'description' => 'Test Description 2',
+            'author' => 'Second Author'
+          }
+        ]
+      )
+    end
+
+    it 'has a max limit of 100' do
+      expect(Book).to receive(:limit).with(100).and_call_original
+      get '/api/v1/books', params: { limit: 999 }
+      expect(response).to have_http_status(:ok)
     end
   end
 
