9ac6dc230ed5dae89441c6a417030930825d6fd9
Oray KURT <oraykt@gmail.com>
2021-02-11 09:27:12 +0100

    Internal Job perform tested

--- spec/jobs/update_sku_job_spec.rb
+++ spec/jobs/update_sku_job_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe UpdateSkuJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'calls SKU service with correct params' do
+    allow(Net::HTTP).to receive(:start).and_return(true)
+    described_class.perform_now('eloquent ruby', 'perfect')
+  end
 end
