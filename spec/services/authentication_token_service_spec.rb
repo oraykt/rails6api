d1858e34e521701ca23450188b3ddbfda6d0afe5
Oray KURT <oraykt@gmail.com>
2021-02-11 10:20:02 +0100

    AuthenticationTokenService Test.v.2

--- spec/services/authentication_token_service_spec.rb
+++ spec/services/authentication_token_service_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe AuthenticationTokenService, type: :request do
   describe '.call' do
     it 'returns an authentication token' do
+      expect(described_class.call).to eql('123')
     end
   end
 end
