0dc2b1cd7776d37562374539afd9f1e4771237eb
Oray KURT <oraykt@gmail.com>
2021-02-11 09:48:38 +0100

    /authenticate authentication#create Test.v.2

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :books, only: %i[index create destroy]
+
+      post 'authenticate', to: 'authentication#create'
     end
   end
 end
