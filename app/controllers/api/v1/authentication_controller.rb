b20a42b846801ff04abbedc2b5d55af377d06939
Oray KURT <oraykt@gmail.com>
2021-02-11 09:51:35 +0100

    /authenticate authentication#create Test.v.3

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 module Api
   module V1
     class AuthenticationController < ApplicationController
-      def create; end
+      def create
+        render json: { token: '123' }, status: :created
+      end
     end
   end
 end
--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -1,9 +1,16 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 describe 'Authentication', type: :request do
   describe 'POST /authenticate' do
     it 'authenticates the client' do
-      post '/api/v1/authenticate'
+      post '/api/v1/authenticate', params: { username: 'test_user', password: 'test_passw0rd.' }
+
+      expect(response).to have_http_status(:created)
+      expect(response_body).to eql({
+                                     'token' => '123'
+                                   })
     end
   end
 end
