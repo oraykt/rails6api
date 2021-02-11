f0c9f1f64d4ddd9de498d20f4698f6800c0caeae
Oray KURT <oraykt@gmail.com>
2021-02-11 09:33:34 +0100

    Internal Job tested in Controller_spec

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -3,8 +3,28 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::BooksController, type: :controller do
-  it 'has a max limit of 100' do
-    expect(Book).to receive(:limit).with(100).and_call_original
-    get :index, params: { limit: 999 }
+  describe 'GET index' do
+    it 'has a max limit of 100' do
+      expect(Book).to receive(:limit).with(100).and_call_original
+      get :index, params: { limit: 999 }
+    end
+  end
+
+  describe 'POST create' do
+    let(:book_title) { 'eloquent ruby' }
+    let(:book_description) { 'perfect' }
+    let(:author_name) { 'Test Author' }
+    it 'calls UpdateSkuJob with correct params' do
+      expect(UpdateSkuJob).to receive(:perform_later).with(book_title, book_description)
+      post :create, params: {
+        author: {
+          name: author_name
+        },
+        book: {
+          title: book_title,
+          description: book_description
+        }
+      }
+    end
   end
 end
--- spec/jobs/update_sku_job_spec.rb
+++ spec/jobs/update_sku_job_spec.rb
@@ -1,8 +1,19 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe UpdateSkuJob, type: :job do
-  it 'calls SKU service with correct params' do
+  let(:book_title) { 'eloquent ruby' }
+  let(:book_description) { 'perfect' }
+
+  before do
     allow(Net::HTTP).to receive(:start).and_return(true)
-    described_class.perform_now('eloquent ruby', 'perfect')
+  end
+
+  it 'calls SKU service with correct params' do
+    expect_any_instance_of(Net::HTTP::Post).to receive(:body=).with(
+      { sku: '123', title: book_title, description: book_description }.to_json
+    )
+    described_class.perform_now(book_title, book_description)
   end
 end
