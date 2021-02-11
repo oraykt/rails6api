78020850c91cf90c8ee065e416f7567f538343f2
Oray KURT <oraykt@gmail.com>
2021-02-11 10:57:59 +0100

    AuthenticationTokenService Test.v.3.1

--- spec/services/authentication_token_service_spec.rb
+++ spec/services/authentication_token_service_spec.rb
@@ -2,17 +2,16 @@ require 'rails_helper'
 
 describe AuthenticationTokenService, type: :request do
   describe '.call' do
-
+    let(:token) {described_class.call(1)}
     it 'returns an authentication token' do
-      token = described_class.call
       decoded_token = JWT.decode(
         token,
         described_class::HMAC_SECRET,
         true,
         { algorithm: described_class::ALGORITHM_TYPE })
       expect(decoded_token).to eql([
-                                            {"test"=>"blah"},
-                                            {"alg"=>described_class::ALGORITHM_TYPE}
+                                            { "user_id"=>1 },
+                                            { "alg"=>described_class::ALGORITHM_TYPE }
                                           ])
     end
   end
