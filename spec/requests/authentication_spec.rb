0838f7ee4ab6865da7e8889c8ee51010d1d9dff7
Oray KURT <oraykt@gmail.com>
2021-02-11 11:37:17 +0100

    Authentication Test.v.3.3

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -4,10 +4,10 @@ require 'rails_helper'
 
 describe 'Authentication', type: :request do
   describe 'POST /authenticate' do
-    let(:user) { FactoryBot.create(:user, username: 'test_user') }
+    let(:user) { FactoryBot.create(:user, username: 'test_user', password: 'test_passw0rd.') }
     let(:token) {AuthenticationTokenService.call(1)}
     it 'authenticates the client' do
-      post '/api/v1/authenticate', params: { username: user.username, password: 'test_passw0rd.' }
+      post '/api/v1/authenticate', params: { username: user.username, password: user.password }
 
       expect(response).to have_http_status(:created)
       expect(response_body).to eql({
@@ -15,7 +15,7 @@ describe 'Authentication', type: :request do
                                    })
     end
     it 'returns error when username is missing' do
-      post '/api/v1/authenticate', params: { password: 'test_passw0rd.' }
+      post '/api/v1/authenticate', params: { password: user.password }
 
       expect(response).to have_http_status(:unprocessable_entity)
       # expect(response_body).to eql({
@@ -30,6 +30,13 @@ describe 'Authentication', type: :request do
       # expect(response_body).to eql({
       #                                'error' => 'param is missing or the value is empty: username'
       #                              })
+    end
+
+    it 'returns error when password is incorrect' do
+      post '/api/v1/authenticate', params: { username: user.username, password: 'incorrect_password' }
+
+      expect(response).to have_http_status(:unauthorized)
+
     end
   end
 end
