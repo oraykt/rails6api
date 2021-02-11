24eb6cba68e8699f37ee5ee78303e534b6ac188f
Oray KURT <oraykt@gmail.com>
2021-02-11 11:31:42 +0100

    Migration-> User.password_digest | User.has_secure_password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_11_094556) do
+ActiveRecord::Schema.define(version: 2021_02_11_102926) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2021_02_11_094556) do
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "books", "authors"
